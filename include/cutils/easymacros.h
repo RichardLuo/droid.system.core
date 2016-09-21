03f094c461c349fb035cd30ef1c49a5c3a09baa0
Richard Luo <cdominant7@gmail.com>
2016-09-21 14:46:51 +0800

    added LOG_EXIT, as an assert function

--- include/cutils/easymacros.h
+++ include/cutils/easymacros.h
@@ -79,6 +79,12 @@
         }                                           \
     } while (0)
 
+#define LOG_EXIT(info_fmt, ...)                 \
+    do {                                        \
+        LOGFL(info_fmt, ## __VA_ARGS__);        \
+        LOG_ALWAYS_FATAL(" CRASHED HERE!!");    \
+    } while (0)
+
 #define LOG_IF_EXECUTE(cond,code,info_fmt, ...) \
     do {                                        \
         if ((cond)) {                           \
